93bb990efefd3011e9b50fd3ebf2f54ad4ccb68d
Александр Азизов <alexandr.azizov@gmail.com>
2021-02-25 23:58:00 +0300

    Line

---
+++ Supelevator/Views/Windows/CalcSmokeCorridor1.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Supelevator.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для CalcSmokeCorridor1.xaml
+    /// </summary>
+    public partial class CalcSmokeCorridor1 : Window
+    {
+        public CalcSmokeCorridor1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public int mode;
+
+
+
+        List<double> listPointsX = new List<double>();
+        List<double> listPointsY = new List<double>();
+        Point point1 = new Point();
+        Point point2 = new Point();
+        bool draw;
+
+
+        private void RibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserControl_Loaded(this, e);
+            var window = Window.GetWindow(this);
+            window.KeyDown += HandleKeyPress;
+
+
+        }
+
+
+        public void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var window = Window.GetWindow(this);
+            //window.KeyDown += HandleKeyPress;
+            //window.KeyDown -= HandleKeyPress;
+        }
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Q)
+                mode = 2;
+            else if (e.Key == Key.W)
+                mode = 3;
+            else if (e.Key == Key.E)
+                mode = 1;
+        }
+        protected void UnRegister()
+        {
+            var window = Window.GetWindow(this);
+            window.KeyDown -= HandleKeyPress;
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            point1 = Mouse.GetPosition(Canvas);
+            listPointsX.Add(point1.X);
+            listPointsY.Add(point1.Y);
+            draw = true;
+
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(draw)
+            {
+                Point p = Mouse.GetPosition(Canvas);
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            draw = false;
+            if (draw == false)
+            {
+                point2 = Mouse.GetPosition(Canvas);
+                listPointsX.Add(point2.X);
+                listPointsY.Add(point2.Y);
+                DrawLine(point1, point2, true);
+            }
+        }
+
+        public void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Q)
+                mode = 2;
+            else if (e.Key == Key.W)
+                mode = 3;
+            else if (e.Key == Key.E)
+                mode = 1;
+        }
+
+        public void DrawLine(Point start, Point stop, bool move)
+        {
+            Line line = new Line();
+            line.Stroke = Brushes.White;
+            line.StrokeThickness = 1;
+            
+            switch (mode)
+            {
+                case 1: // стандартная линия
+                    line.X1 = start.X;
+                    line.Y1 = start.Y;
+                    line.X2 = stop.X;
+                    line.Y2 = stop.Y;
+                    Canvas.Children.Add(line);
+                    break;
+                case 2: // горизонтальная линия
+                    line.X1 = start.X;
+                    line.Y1 = start.Y;
+                    line.X2 = stop.X;
+                    line.Y2 = line.Y1;
+                    Canvas.Children.Add(line);
+                    break;
+                case 3: // вертикальная линия
+                    line.Y1 = start.Y;
+                    line.X1 = start.X;
+                    line.Y2 = stop.Y;
+                    line.X2 = line.X1;
+                    Canvas.Children.Add(line);
+                    break;
+            }
+        }
+
+        private void Canvas_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Line line = new Line();
+            line.Stroke = Brushes.White;
+            line.StrokeThickness = 2;
+
+            if (e.Key == Key.LeftShift)
+            {
+                point1.X = Mouse.GetPosition(Canvas).X - Canvas.Margin.Left;
+                point2.X = Mouse.GetPosition(Canvas).X - Canvas.Margin.Left;
+                point1.Y = Mouse.GetPosition(Canvas).Y - Canvas.Margin.Top;
+                point2.Y = point1.Y;
+                line.X1 = point1.X;
+                line.X2 = point2.X;
+                line.Y1 = point1.Y;
+                line.Y2 = line.Y1;
+                Canvas.Children.Add(line);
+            }
+        }
+
+
+    }
+}
---
+++ Tests/ArcLineTest/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcLineTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool Move;
+        public int Mode = 1;
+        int [] lines = new int[300];
+
+        List<Point> points = new List<Point>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Point currentPoint1 = new Point();
+        Point currentPoint2 = new Point();
+
+
+
+
+        private void paintLine_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState==MouseButtonState.Pressed)
+            {
+                currentPoint1 = e.GetPosition(this);
+                
+
+
+            }
+        }
+
+        private void paintLine_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Released)
+            {
+                currentPoint2 = e.GetPosition(this);
+                Move = true;
+            }    
+        }
+        // устанаваливаем ружим рисования линии
+        //private void SetLinePosition(int mode)
+        //{
+        //    Line line = new Line();
+        //    if (Line == null) return;
+        //    switch (mode)
+        //    {
+        //        case 1: //стандарт
+        //            Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+        //            Line.Y2 = Mouse.GetPosition(this).Y - canvas.Margin.Top;
+        //            break;
+
+        //        case 2: //по углом 45 градусов слева
+        //            Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+        //            Line.Y2 = Line.Y1 - (Line.X1 - Mouse.GetPosition(this).X + canvas.Margin.Left);
+        //            break;
+
+        //        case 3: //по углом 45 градусов справа
+        //            Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+        //            Line.Y2 = Line.Y1 + (Line.X1 - Mouse.GetPosition(this).X + canvas.Margin.Left);
+        //            break;
+
+        //        case 4: //по вертикале
+        //            Line.X2 = Line.X1;
+        //            Line.Y2 = Mouse.GetPosition(this).Y - canvas.Margin.Top;
+        //            break;
+
+        //        case 5: //по горизонтале
+        //            Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+        //            Line.Y2 = Line.Y1;
+        //            break;
+        //    }
+
+
+
+
+
+            private void paintLine_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed)
+            {
+                currentPoint2 = e.GetPosition(paintLine);
+                DrawLine(currentPoint1, currentPoint2, true);
+
+            }  
+        }
+        private void DrawLine(Point start, Point stop, bool move)
+        {
+            Line line = new Line();
+            line.Stroke = Brushes.Green;
+            line.StrokeThickness = 5;
+            line.X1 = start.X;
+            line.Y1 = start.Y;
+            line.X2 = stop.X;
+            line.Y2 = stop.Y;
+
+                if (!move)
+                    paintLine.Children.Add(line);
+
+                else
+                    if (paintLine.Children.Count > 2 && !Move)
+                {
+
+                    paintLine.Children.RemoveAt(paintLine.Children.Count - 1);
+                    paintLine.Children.Add(line);
+
+
+                }
+                else
+                {
+                    paintLine.Children.Add(line);
+                    Move = false;
+                }
+            
+
+
+        }
+
+    }
+}
